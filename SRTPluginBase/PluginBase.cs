7552e92a8a1c1ee3ad617d2b42bcd4fdcaf55f02
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2020-10-02 20:33:36 -0500

    2.4.3: Fixed a flaw causing it to detect the config file location as SRTPluginBase.cfg for plugins built against 2.4.1-logic.

--- SRTPluginBase/PluginBase.cs
+++ SRTPluginBase/PluginBase.cs
@@ -17,14 +17,15 @@ namespace SRTPluginBase
         private string GetConfigFile(Assembly a) => Path.Combine(new FileInfo(a.Location).DirectoryName, string.Format("{0}.cfg", Path.GetFileNameWithoutExtension(new FileInfo(a.Location).Name)));
         private JsonSerializerOptions jso = new JsonSerializerOptions() { AllowTrailingCommas = true, ReadCommentHandling = JsonCommentHandling.Skip, WriteIndented = true };
 
-        public virtual T LoadConfiguration<T>() where T : class, new() => LoadConfiguration<T>(null);
-        public virtual T LoadConfiguration<T>(IPluginHostDelegates hostDelegates) where T : class, new()
+        public virtual T LoadConfiguration<T>() where T : class, new() => LoadConfiguration<T>(null, GetConfigFile(Assembly.GetCallingAssembly()));
+        public virtual T LoadConfiguration<T>(IPluginHostDelegates hostDelegates) where T : class, new() => LoadConfiguration<T>(hostDelegates, GetConfigFile(Assembly.GetCallingAssembly()));
+        private T LoadConfiguration<T>(IPluginHostDelegates hostDelegates, string configFile) where T : class, new()
         {
-            FileInfo configFile = new FileInfo(GetConfigFile(Assembly.GetCallingAssembly()));
             try
             {
-                if (configFile.Exists)
-                    using (FileStream fs = new FileStream(configFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                FileInfo configFileInfo = new FileInfo(configFile);
+                if (configFileInfo.Exists)
+                    using (FileStream fs = new FileStream(configFileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
                         return JsonSerializer.DeserializeAsync<T>(fs, jso).Result;
                 else
                     return new T(); // File did not exist, just return a new instance.
@@ -40,15 +41,14 @@ namespace SRTPluginBase
             }
         }
 
-        public virtual void SaveConfiguration<T>(T configuration) where T : class, new() => SaveConfiguration<T>(configuration, null);
-        public virtual void SaveConfiguration<T>(T configuration, IPluginHostDelegates hostDelegates) where T : class, new()
+        public virtual void SaveConfiguration<T>(T configuration) where T : class, new() => SaveConfiguration<T>(configuration, null, GetConfigFile(Assembly.GetCallingAssembly()));
+        public virtual void SaveConfiguration<T>(T configuration, IPluginHostDelegates hostDelegates) where T : class, new() => SaveConfiguration<T>(configuration, hostDelegates, GetConfigFile(Assembly.GetCallingAssembly()));
+        private void SaveConfiguration<T>(T configuration, IPluginHostDelegates hostDelegates, string configFile) where T : class, new()
         {
-            // Only save if configuration is not null.
-            if (configuration != null)
+            if (configuration != null) // Only save if configuration is not null.
             {
                 try
                 {
-                    string configFile = GetConfigFile(Assembly.GetCallingAssembly());
                     using (FileStream fs = new FileStream(configFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete))
                         JsonSerializer.SerializeAsync<T>(fs, configuration, jso).Wait();
                 }

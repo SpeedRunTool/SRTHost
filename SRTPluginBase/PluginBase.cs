8ec5a9b1594132333bc54b38fdcc7684ffb0021f
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2020-10-02 20:05:59 -0500

    2.4.2: Fix an issue where saving a configuration entry as null would delete user settings. Also added additional sanity checks to both Load and Save config.

--- SRTPluginBase/PluginBase.cs
+++ SRTPluginBase/PluginBase.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -16,25 +17,50 @@ namespace SRTPluginBase
         private string GetConfigFile(Assembly a) => Path.Combine(new FileInfo(a.Location).DirectoryName, string.Format("{0}.cfg", Path.GetFileNameWithoutExtension(new FileInfo(a.Location).Name)));
         private JsonSerializerOptions jso = new JsonSerializerOptions() { AllowTrailingCommas = true, ReadCommentHandling = JsonCommentHandling.Skip, WriteIndented = true };
 
-        public virtual T LoadConfiguration<T>() where T : class, new()
+        public virtual T LoadConfiguration<T>() where T : class, new() => LoadConfiguration<T>(null);
+        public virtual T LoadConfiguration<T>(IPluginHostDelegates hostDelegates) where T : class, new()
         {
-            string configFile = GetConfigFile(Assembly.GetCallingAssembly());
+            FileInfo configFile = new FileInfo(GetConfigFile(Assembly.GetCallingAssembly()));
             try
             {
-                using (FileStream fs = new FileStream(configFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
-                    return JsonSerializer.DeserializeAsync<T>(fs, jso).Result;
+                if (configFile.Exists)
+                    using (FileStream fs = new FileStream(configFile.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                        return JsonSerializer.DeserializeAsync<T>(fs, jso).Result;
+                else
+                    return new T(); // File did not exist, just return a new instance.
             }
-            catch
+            catch (Exception ex)
             {
-                return new T();
+                if (hostDelegates != null)
+                {
+                    try { hostDelegates.ExceptionMessage.Invoke(ex); }
+                    catch { }
+                }
+                return new T(); // An exception occurred when reading the file, return a new instance.
             }
         }
 
-        public virtual void SaveConfiguration<T>(T configuration) where T : class, new()
+        public virtual void SaveConfiguration<T>(T configuration) where T : class, new() => SaveConfiguration<T>(configuration, null);
+        public virtual void SaveConfiguration<T>(T configuration, IPluginHostDelegates hostDelegates) where T : class, new()
         {
-            string configFile = GetConfigFile(Assembly.GetCallingAssembly());
-            using (FileStream fs = new FileStream(configFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete))
-                JsonSerializer.SerializeAsync<T>(fs, configuration, jso).Wait();
+            // Only save if configuration is not null.
+            if (configuration != null)
+            {
+                try
+                {
+                    string configFile = GetConfigFile(Assembly.GetCallingAssembly());
+                    using (FileStream fs = new FileStream(configFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete))
+                        JsonSerializer.SerializeAsync<T>(fs, configuration, jso).Wait();
+                }
+                catch (Exception ex)
+                {
+                    if (hostDelegates != null)
+                    {
+                        try { hostDelegates.ExceptionMessage.Invoke(ex); }
+                        catch { }
+                    }
+                }
+            }
         }
     }
 }

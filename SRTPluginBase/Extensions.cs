a4d6e8ec1aa8ca54ad990cf32cfdaef3bf3dda2b
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2021-07-09 18:10:12 -0500

    3.0.0.0: .NET 5.0 upgrade (#1)
    * 3.0.0.0 changes
    ---
    .NET 5.0 for Host
    .NET Standard 2.1 for Plugin Base
    Adjustments to signing info showing
    Publish changes to be self-contained, R2R and single file
    
    * Adjustments to publish signing event
    
    * Removed an environment variable check
    
    * Added --Provider
    
    * Added ability to load and save config directly by file path. Also made some methods public.
    
    * Made GetConfigFile public.
    
    * Rearrange some stuff in PluginBase/Extensions.
    
    * Changed to cast so it throws exception when a version mismatch occurs.
    
    * Adjusted gitignore to allow my publish profiles. Added my publish profiles.

--- SRTPluginBase/Extensions.cs
+++ SRTPluginBase/Extensions.cs
@@ -1,4 +1,9 @@
-﻿namespace SRTPluginBase
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+
+namespace SRTPluginBase
 {
     public static class Extensions
     {
@@ -23,6 +28,60 @@
 
             // We made it past the for loop, we're equals!
             return true;
+        }
+
+        public static readonly JsonSerializerOptions JSO = new JsonSerializerOptions() { AllowTrailingCommas = true, ReadCommentHandling = JsonCommentHandling.Skip, WriteIndented = true };
+        public static string GetConfigFile(this Assembly a) => Path.Combine(new FileInfo(a.Location).DirectoryName, string.Format("{0}.cfg", Path.GetFileNameWithoutExtension(new FileInfo(a.Location).Name)));
+
+        public static T LoadConfiguration<T>() where T : class, new() => LoadConfiguration<T>(null);
+        public static T LoadConfiguration<T>(IPluginHostDelegates hostDelegates = null) where T : class, new() => LoadConfiguration<T>(null, hostDelegates);
+        public static T LoadConfiguration<T>(string? configFile = null, IPluginHostDelegates hostDelegates = null) where T : class, new()
+        {
+            if (configFile == null)
+                configFile = GetConfigFile(typeof(T).Assembly);
+
+            try
+            {
+                if (File.Exists(configFile))
+                    using (FileStream fs = new FileStream(configFile, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete))
+                        return JsonSerializer.DeserializeAsync<T>(fs, JSO).Result;
+                else
+                    return new T(); // File did not exist, just return a new instance.
+            }
+            catch (Exception ex)
+            {
+                if (hostDelegates != null)
+                {
+                    try { hostDelegates.ExceptionMessage.Invoke(ex); }
+                    catch { }
+                }
+                return new T(); // An exception occurred when reading the file, return a new instance.
+            }
+        }
+
+        public static void SaveConfiguration<T>(this T configuration) where T : class, new() => SaveConfiguration<T>(configuration, null);
+        public static void SaveConfiguration<T>(this T configuration, IPluginHostDelegates hostDelegates = null) where T : class, new() => SaveConfiguration<T>(configuration, null, hostDelegates);
+        public static void SaveConfiguration<T>(this T configuration, string? configFile = null, IPluginHostDelegates hostDelegates = null) where T : class, new()
+        {
+            if (configFile == null)
+                configFile = GetConfigFile(typeof(T).Assembly);
+
+            if (configuration != null) // Only save if configuration is not null.
+            {
+                try
+                {
+                    using (FileStream fs = new FileStream(configFile, FileMode.Create, FileAccess.Write, FileShare.ReadWrite | FileShare.Delete))
+                        JsonSerializer.SerializeAsync<T>(fs, configuration, JSO).Wait();
+                }
+                catch (Exception ex)
+                {
+                    if (hostDelegates != null)
+                    {
+                        try { hostDelegates.ExceptionMessage.Invoke(ex); }
+                        catch { }
+                    }
+                }
+            }
         }
     }
 }

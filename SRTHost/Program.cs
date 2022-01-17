0aac7429b18eda1ebd7ed3fcf12c93873128b714
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2022-01-17 14:32:31 -0600

    Add appsettings to allow users to configure what port the program listens on.

--- SRTHost/Program.cs
+++ SRTHost/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SRTHost.LoggerImplementations;
 using System;
-using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
 

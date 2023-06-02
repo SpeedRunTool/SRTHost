0aa9707fc01e19442b529aa80156ef978e9c9bd7
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2023-06-01 19:24:20 -0500

    Reorganized settings saving code.

--- src/SRTHost/Helpers.cs
+++ src/SRTHost/Helpers.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using MudBlazor;
 using SRTPluginBase;
 

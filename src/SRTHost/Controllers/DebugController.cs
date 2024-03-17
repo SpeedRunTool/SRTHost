312b790947cb4eec00571ed6d8e06c22aa237804
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2024-03-17 03:30:40 -0500

    Fixed Plugins page with new json format.

--- src/SRTHost/Controllers/DebugController.cs
+++ src/SRTHost/Controllers/DebugController.cs
@@ -6,7 +6,6 @@ using SRTPluginBase.Interfaces;
 using System;
 using System.Diagnostics;
 using System.Text.Json;
-using static MudBlazor.CategoryTypes;
 
 namespace SRTHost.Controllers
 {
@@ -36,7 +35,7 @@ namespace SRTHost.Controllers
             return new JsonResult(
                 new MainJson()
                 {
-                    Hosts = new IMainHostEntry[]
+                    Hosts = new MainHostEntry[]
                     {
                         new MainHostEntry()
                         {
@@ -44,44 +43,45 @@ namespace SRTHost.Controllers
                             DisplayName = "SRT Host",
                             Description = "A plugin host for various informational SpeedRun Tools.",
                             RepoURL = new Uri("https://github.com/SpeedRunTool/SRTHost"),
-                            ManifestURL = new Uri("https://raw.githubusercontent.com/SpeedRunTool/SRTHost/develop/manifest.json")
+                            ManifestURL = new Uri("https://127.0.0.1:7192/api/v1/Debug/GenerateManifestHost", UriKind.Absolute)
+                            //ManifestURL = new Uri("https://raw.githubusercontent.com/SpeedRunTool/SRTHost/main/manifest.json")
+                        },
+                        new MainHostEntry()
+                        {
+                            Name = "SRTHost",
+                            DisplayName = "SRT Host (Beta)",
+                            Description = "A plugin host for various informational SpeedRun Tools. This is a beta version and may not work as intended.",
+                            RepoURL = new Uri("https://github.com/SpeedRunTool/SRTHost"),
+                            ManifestURL = new Uri("https://127.0.0.1:7192/api/v1/Debug/GenerateManifestHost", UriKind.Absolute)
+                            //ManifestURL = new Uri("https://raw.githubusercontent.com/SpeedRunTool/SRTHost/develop/manifest.json")
                         }
                     },
-                    Plugins = new IMainPluginEntry[]
+                    Plugins = new MainPluginEntry[]
                     {
-                            new MainPluginEntry()
-                            {
-                                Name = "SRTProducerTest",
-                                DisplayName = "Test Producer",
-                                Description = "A plugin for Test that produces values from in-game memory for consumers.",
-                                Platform = MainPluginPlatformEnum.x64,
-                                Type = MainPluginTypeEnum.Producer,
-                                Tags = new string[] { "Producer" },
-                                RepoURL = new Uri("https://github.com/TestAuthor1/SRTProducerTest1"),
-                                ManifestURL = new Uri("https://raw.githubusercontent.com/TestAuthor1/SRTProducerTest1/main/manifest.json")
-                            },
-                            new MainPluginEntry()
-                            {
-                                Name = "SRTConsumerTestUIDirectX",
-                                DisplayName = "Test Consumer (DirectX)",
-                                Description = "A plugin for Test that displays in-game data within a DirectX Overlay.",
-                                Platform = MainPluginPlatformEnum.x64,
-                                Type = MainPluginTypeEnum.Consumer,
-                                Tags = new string[] { "Consumer", "UI", "Overlay", "DirectX" },
-                                RepoURL = new Uri("https://github.com/TestAuthor1/SRTConsumerTest1"),
-                                ManifestURL = new Uri("https://raw.githubusercontent.com/TestAuthor1/SRTConsumerTest1/main/manifest.json")
-                            },
-                            new MainPluginEntry()
-                            {
-                                Name = "SRTConsumerTestUIWinForms",
-                                DisplayName = "Test Consumer (WinForms)",
-                                Description = "A plugin for Test that displays in-game data within a Win32 WinForm.",
-                                Platform = MainPluginPlatformEnum.x64,
-                                Type = MainPluginTypeEnum.Consumer,
-                                Tags = new string[] { "Consumer", "UI", "WinForms" },
-                                RepoURL = new Uri("https://github.com/TestAuthor1/SRTConsumerTest2"),
-                                ManifestURL = new Uri("https://raw.githubusercontent.com/TestAuthor1/SRTConsumerTest2/main/manifest.json")
-                            },
+                        new MainPluginEntry()
+                        {
+                            Name = "SRTPluginProviderRE2",
+                            DisplayName = "Resident Evil 2 (2019) (Beta)",
+                            Description = "A producer plugin for Resident Evil 2 (2019) that produces values from in-game memory for consumers.",
+                            Platform = MainPluginPlatformEnum.x64,
+                            Type = MainPluginTypeEnum.Producer,
+                            Tags = new string[] { "Producer" },
+                            RepoURL = new Uri("https://github.com/SpeedrunTooling/SRTPluginProviderRE2"),
+                            ManifestURL = new Uri("https://127.0.0.1:7192/api/v1/Debug/GenerateManifestPlugin", UriKind.Absolute)
+                            //ManifestURL = new Uri("https://raw.githubusercontent.com/SpeedrunTooling/SRTPluginProviderRE2/develop/manifest.json")
+                        },
+                        new MainPluginEntry()
+                        {
+                            Name = "SRTPluginUIRE2DirectXOverlay",
+                            DisplayName = "Resident Evil 2 (2019) DirectX Overlay (Beta)",
+                            Description = "A consumer plugin for Resident Evil 2 (2019) that displays in-game data within a DirectX Overlay.",
+                            Platform = MainPluginPlatformEnum.x64,
+                            Type = MainPluginTypeEnum.Consumer,
+                            Tags = new string[] { "Consumer", "UI", "Overlay", "DirectX" },
+                            RepoURL = new Uri("https://github.com/SpeedrunTooling/SRTPluginUIRE2DirectXOverlay"),
+                            ManifestURL = new Uri("https://127.0.0.1:7192/api/v1/Debug/GenerateManifestPlugin", UriKind.Absolute)
+                            //ManifestURL = new Uri("https://raw.githubusercontent.com/SpeedrunTooling/SRTPluginUIRE2DirectXOverlay/develop/manifest.json")
+                        },
                     }
                 },
                 new JsonSerializerOptions()
@@ -106,17 +106,32 @@ namespace SRTHost.Controllers
                     {
                         "Squirrelies"
                     },
-                    Releases = new IManifestReleaseJson[]
+                    Releases = new ManifestReleaseJson[]
                     {
                         new ManifestReleaseJson()
                         {
-                            Version = "1.0.0.1",
-                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/1.0.0.1/SRTHost-v1.0.0.1.zip")
+                            Version = "3.1.0.1",
+                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/3.1.0.1/SRTHost-v3.1.0.1.zip")
                         },
                         new ManifestReleaseJson()
                         {
-                            Version = "1.0.0.0",
-                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/1.0.0.0/SRTHost-v1.0.0.0.zip")
+                            Version = "3.0.0.3",
+                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/3.0.0.3/SRTHost-v3.0.0.3.zip")
+                        },
+                        new ManifestReleaseJson()
+                        {
+                            Version = "3.0.0.2",
+                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/3.0.0.2/SRTHost-v3.0.0.2.zip")
+                        },
+                        new ManifestReleaseJson()
+                        {
+                            Version = "3.0.0.1",
+                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/3.0.0.1/SRTHost-v3.0.0.1.zip")
+                        },
+                        new ManifestReleaseJson()
+                        {
+                            Version = "3.0.0.0",
+                            DownloadURL = new Uri("https://github.com/SpeedRunTool/SRTHost/releases/download/3.0.0.0/SRTHost-v3.0.0.0.zip")
                         }
                     }
                 },
@@ -135,20 +150,20 @@ namespace SRTHost.Controllers
                 {
                     Contributors = new string[]
                     {
-                        "TestAuthor1",
-                        "TestAuthor2"
+                        "Squirrelies",
+                        "VideoGameRoulette"
                     },
                     Releases = new ManifestReleaseJson[]
                     {
                         new ManifestReleaseJson()
                         {
-                            Version = "1.0.0.1",
-                            DownloadURL = new Uri("https://github.com/TestAuthor1/SRTConsumerTest1/releases/download/1.0.0.1/SRTConsumerTest1-v1.0.0.1.zip")
+                            Version = "2.0.2.0",
+                            DownloadURL = new Uri("https://github.com/SpeedrunTooling/SRTPluginProviderRE2/releases/download/2.0.2.0/SRTPluginProviderRE2-v2.0.2.0.zip")
                         },
                         new ManifestReleaseJson()
                         {
-                            Version = "1.0.0.0",
-                            DownloadURL = new Uri("https://github.com/TestAuthor1/SRTConsumerTest1/releases/download/1.0.0.0/SRTConsumerTest1-v1.0.0.0.zip")
+                            Version = "2.0.0.8",
+                            DownloadURL = new Uri("https://github.com/SpeedrunTooling/SRTPluginProviderRE2/releases/download/2.0.0.8/SRTPluginProviderRE2-v2.0.0.8.zip")
                         }
                     }
                 },
